e94fa9b8da431a0a040b3ca16f3754a1d81e2563
bachtung144 <bachtung144007@gmail.com>
2022-12-16 15:42:11 +0700

    Pagination with External Package

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,11 @@ import './bootstrap';
 
 import { createApp } from 'vue/dist/vue.esm-bundler';
 import PostsIndex from "./components/Posts/Index.vue"
+import { TailwindPagination } from 'laravel-vue-pagination';
 
 const app = createApp({})
+
 app.component('posts-index', PostsIndex)
+app.component('Pagination', TailwindPagination)
+
 app.mount('#app')
--- resources/js/composabales/posts.js
+++ resources/js/composabales/posts.js
@@ -1,10 +1,10 @@
 import {ref} from "vue";
 
 export default function usePosts() {
-    const posts = ref([])
-    const getPosts = async () => {
-        axios.get('/api/posts')
-            .then(responses => posts.value = responses.data.data)
+    const posts = ref({})
+    const getPosts = async (page= 1) => {
+        axios.get('/api/posts?page=' + page)
+            .then(responses => posts.value = responses.data)
     }
 
     return {posts, getPosts}
