f7b1e86091e51b234971c7a77663e9d702f8c85e
bachtung144 <bachtung144007@gmail.com>
2022-12-17 14:33:47 +0700

    Form Submit: Storing the Data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -28,5 +28,12 @@ class PostController extends Controller
             ->paginate(10);
 
         return PostResource::collection($posts);
+    }
+
+    public function store(StorePostRequest $request)
+    {
+        $post = Post::create($request->validated());
+
+        return new PostResource($post);
     }
 }
