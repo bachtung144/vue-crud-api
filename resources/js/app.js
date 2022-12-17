a0309adae1bf71879a166fbc5c4187939cbd6380
bachtung144 <bachtung144007@gmail.com>
2022-12-17 09:43:13 +0700

    Routing: Navigation Links and Posts Create Page

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,25 @@ import './bootstrap';
 
 import { createApp } from 'vue/dist/vue.esm-bundler';
 import PostsIndex from "./components/Posts/Index.vue"
+import PostsCreate from "./components/Posts/Create.vue"
 import { TailwindPagination } from 'laravel-vue-pagination';
+import {createRouter, createWebHistory} from "vue-router";
+import App from './layouts/App.vue'
 
-const app = createApp({})
+const routes = [
+    {path: '/', component: PostsIndex},
+    {path: '/posts/create', component: PostsCreate}
+]
 
-app.component('posts-index', PostsIndex)
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+const app = createApp(App)
+
+app.use(router)
+
 app.component('Pagination', TailwindPagination)
 
 app.mount('#app')
