ba9f5345d453bcbcf19e747a3283b1d411457bd8
bachtung144 <bachtung144007@gmail.com>
2022-12-17 10:33:48 +0700

    Vue.js Routing: Route Names and Separate File

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,9 @@
 import './bootstrap';
 
 import { createApp } from 'vue/dist/vue.esm-bundler';
-import PostsIndex from "./components/Posts/Index.vue"
-import PostsCreate from "./components/Posts/Create.vue"
+import {router} from "./routes";
 import { TailwindPagination } from 'laravel-vue-pagination';
-import {createRouter, createWebHistory} from "vue-router";
 import App from './layouts/App.vue'
-
-const routes = [
-    {path: '/', component: PostsIndex},
-    {path: '/posts/create', component: PostsCreate}
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
 
 const app = createApp(App)
 
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'dashboard')->name('dashboard');
+Route::view('/{any?}', 'dashboard')
+    ->name('dashboard')
+    ->where('any','.*');
 
-//Route::middleware('auth')->group(function () {
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//});
-//
-//require __DIR__.'/auth.php';
