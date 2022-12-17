2b488efa6e4e1bdde2416fac1314f01ef0e98cb8
bachtung144 <bachtung144007@gmail.com>
2022-12-17 08:50:07 +0700

    Ordering Data by Clicking Column Titles

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -13,10 +13,40 @@
                 <thead>
                 <tr>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>
+<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>-->
+                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('id')">
+                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'id' }">
+                                ID
+                            </div>
+                            <div class="select-none">
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'id',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'id',
+                                }">&uarr;</span>
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'id',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'id',
+                                }">&darr;</span>
+                            </div>
+                        </div>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>
+<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>-->
+                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('title')">
+                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'title' }">
+                                Title
+                            </div>
+                            <div class="select-none">
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'title',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'title',
+                                }">&uarr;</span>
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'title',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'title',
+                                }">&darr;</span>
+                            </div>
+                        </div>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
                         <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Category</span>
@@ -25,7 +55,22 @@
                         <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>
+<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>-->
+                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('created_at')">
+                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'created_at' }">
+                                Created at
+                            </div>
+                            <div class="select-none">
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'created_at',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'created_at',
+                                }">&uarr;</span>
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'created_at',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'created_at',
+                                }">&darr;</span>
+                            </div>
+                        </div>
                     </th>
                 </tr>
                 </thead>
@@ -52,20 +97,22 @@
 
             <Pagination
                 :data="posts"
-                @pagination-change-page="getPosts"
+                @pagination-change-page="page => getPosts(page, selectedCategory)"
             />
         </div>
     </div>
 </template>
 
 <script>
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 import usePosts from "../../composabales/posts";
 import useCategories from "../../composabales/categories";
 
 export default {
     setup() {
         const selectedCategory = ref('')
+        const orderColumn = ref('created_at')
+        const orderDirection = ref('des')
         const {posts, getPosts} = usePosts()
         const {categories, getCategories} = useCategories()
 
@@ -74,7 +121,26 @@ export default {
             getCategories()
         })
 
-        return { posts, getPosts, categories, getCategories }
+        const updateOrdering = (column) => {
+            orderColumn.value = column
+            orderDirection.value = (orderDirection.value === 'asc') ? 'desc' : 'asc'
+            getPosts(1, selectedCategory.value, orderColumn.value, orderDirection.value)
+        }
+
+        watch(selectedCategory, (current, previous) => {
+            getPosts(1, current)
+        })
+
+        return {
+            posts,
+            getPosts,
+            categories,
+            getCategories,
+            selectedCategory,
+            orderColumn,
+            orderDirection,
+            updateOrdering
+        }
     }
 }
 </script>
