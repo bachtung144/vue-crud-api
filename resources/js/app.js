7f1ae7b1bb0f578225539b472b9d16aa3aeadbb1
bachtung144 <bachtung144007@gmail.com>
2022-12-21 10:45:52 +0700

    SweetAlert for Screen Alert Notifications

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,12 @@ import { createApp } from 'vue/dist/vue.esm-bundler';
 import {router} from "./routes";
 import { TailwindPagination } from 'laravel-vue-pagination';
 import App from './layouts/App.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueSweetalert2);
 
 app.component('Pagination', TailwindPagination)
 
